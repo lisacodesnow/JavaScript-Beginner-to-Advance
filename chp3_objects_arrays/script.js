1fe4b57e87fafd0fe9d9eaf59ca8c1579e4cb6bf
Lisa T <llmt2007@aol.com>
2023-06-26 22:07:55 -0400

    Doing exercises

---
+++ chp3_objects_arrays/script.js
@@ -0,0 +1,8 @@
+shoppingList= ["milk","Bread", "Apple", "milk"];
+console.log(shoppingList,shoppingList.length);
+shoppingList[1] = "Banana";
+console.log(shoppingList,shoppingList.length);
+let list = shoppingList.lastIndexOf("milk");
+shoppingList.sort();
+shoppingList.reverse()
+console.log(shoppingList)
