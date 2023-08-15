e6dff7552d1ab53c60187e6b065762d7a50469e8
Lisa T <llmt2007@aol.com>
2023-08-15 15:44:21 -0400

    Did lesson 5.7

--- ch5_loops/prcch5.js
+++ ch5_loops/prcch5.js
@@ -31,14 +31,94 @@ do{
 
 //Ex: 5.3 Create an array to hold objects
 
-const myWork = [];
-for(x=1; x<10; x++){
-    let stat = x%2 ? true : false
-    let lessonPlan = {
-        name: `Lesson ${x}`,
-        status: stat
-    }
-    myWork.push(lessonPlan)
+/*const myWork = [];
+
+for(let i = 1; i<=10; i++){
+    let stat = i%2 ? true : false;
+    let temp ={
+        name: `Lesson ${i}`,
+        status: stat,
+    };
+    myWork.push(temp)
     console.log(myWork)
+}*/
+
+//Ex: 5.4 Create tabular data with nested arrays
+
+/*const myTalbe = [];
+let rows = 5;
+let columns = 8;
+let counter = 0;
+
+for(let i= 0; i<rows; i++){
+    let firstRow = [];
+    for(let j=0; j<columns; j++){
+       counter ++
+        firstRow.push(counter)
+       
+    }
+    myTalbe.push(firstRow)
+    console.table(myTalbe)
+}*/
+
+//Ex: 5.5
+
+/*const gridArray = [];
+let cells = 64;
+let counter =0;
+let rowArray;
+
+for(let i=0; i<cells+1; i++){
+    if(counter%8 == 0){
+        if(rowArray != undefined){
+            gridArray.push(rowArray)
+        }
+        rowArray = []
+    }
+
+    let counts = counter ++
+    rowArray.push(counts)
 }
 
+console.table(gridArray)*/
+
+//Ex: 5.6
+
+/*let arr = [];
+for(x=0; x<10; x++){
+    arr.push(x)
+}
+
+console.log(arr)
+
+for(i=0; i<arr.length; i++){
+    console.log(arr[i])
+}
+
+
+for(let newArr of arr){
+    console.log(newArr)
+}*/
+
+//Ex: 5.7
+
+let fruit = {
+    apple: "red",
+    banana: "yellow",
+    grape: "pruple"
+}
+
+for(let color in fruit){
+    console.log(color, fruit[color])
+}
+
+let fruits=[
+    "apple", "red", "banana", "yellow", "orange", "grape","purple"
+]
+
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i])
+}
+for(let colors in fruits){
+    console.log(fruits[colors])
+} 
